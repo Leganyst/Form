2b0fd57d08340559d2e6abeb9305ac7104d4aad2
Марк <Leganyst@yandex.ru>
2024-11-07 01:24:31 +0300

    Уточнена архитектура, разбиты базовые файлы-пустышки

---
+++ app/main.py
@@ -0,0 +1,28 @@
+# app/main.py
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from app.core.database import engine, Base
+from app.models import users
+# from app.routers import user
+
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+    await engine.dispose()
+
+
+# @app.on_event("startup")
+# async def startup():
+#     async with engine.begin() as conn:
+#         await conn.run_sync(Base.metadata.create_all)
+
+# @app.on_event("shutdown")
+# async def shutdown():
+#     await engine.dispose()
+
+app = FastAPI(lifespan=lifespan)
+# app.include_router(user.router, prefix="/api")
